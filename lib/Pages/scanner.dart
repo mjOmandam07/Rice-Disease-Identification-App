47f49c59f903a292247b04e286ddcb81d42c2b1c
mjOmandam07 <mjomandam@gmail.com>
2022-02-21 15:54:08 +0800

    Developed the essential components for the scanning feature such as accessing the device camera and selecting images from local directory or gallery.

--- lib/Pages/scanner.dart
+++ lib/Pages/scanner.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rice_disease_detection/Clipper/clipper.dart';
+import 'package:image_picker/image_picker.dart';
 
 class scanner extends StatefulWidget {
   const scanner({Key? key}) : super(key: key);
@@ -11,6 +13,9 @@ class scanner extends StatefulWidget {
 }
 
 class _scannerState extends State<scanner> {
+
+  File ? imageFile;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +45,7 @@ class _scannerState extends State<scanner> {
           Container(
               child: Column(
                 children: [
-                  SizedBox(height: 80),
+                  SizedBox(height: 50),
                   Stack(
                     children: [
                       Container(
@@ -61,7 +66,7 @@ class _scannerState extends State<scanner> {
                       ),
                       Container(
                         alignment: Alignment.topLeft,
-                        margin: EdgeInsets.only(bottom: 50, top: 22, left: 5),
+                        margin: EdgeInsets.only(bottom: 0, top: 22, left: 5),
                         child: Padding(
                           padding: const EdgeInsets.all(0),
                           child: Text(
@@ -80,83 +85,196 @@ class _scannerState extends State<scanner> {
 
                   Expanded(
                     child: Container(
-                      margin: EdgeInsets.only(top: 60),
+                      margin: EdgeInsets.only(top:0),
                       alignment: Alignment.center,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                      child: Column(
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(30, 8.0, 20, 8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  height: 130,
-                                  width: 130,
-                                  child: ElevatedButton(
-                                      onPressed: (){
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                          primary: Colors.white,
-                                          shadowColor: Colors.grey[900],
-                                          elevation: 10,
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.circular(20)
-                                          )
-                                      ),
-                                      child: Image.asset('assets/camera.png')
-                                  ),
+                          if (imageFile != null)
+                            Container(
+                              margin: EdgeInsets.all(12.0),
+                              width: 640,
+                              height: 400,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: Color.fromRGBO(
+                                    3, 135, 96, 0.7686274509803922),
+                                image: DecorationImage(
+                                 image:  FileImage(imageFile!)
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.only(bottom: 8.0, top: 10),
-                                  child: Text(
-                                    'Take Photo',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 15,
-                                      fontFamily: 'Montserrat',
-                                    ),
-                                  ),
-                                ),
-                              ],
+                                border: Border.all(width: 5, color: Colors.black12),
+                                borderRadius: BorderRadius.circular(12.0)
+                              ),
+                            )
+                          else
+                            Container(
+                              margin: EdgeInsets.all(12.0),
+                              width: 640,
+                              height: 400,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                  color: Color.fromRGBO(
+                                      3, 135, 96, 0.7686274509803922),
+                                  border: Border.all(width: 5, color: Colors.black12),
+                                  borderRadius: BorderRadius.circular(12.0)
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(20, 8.0, 8.0, 8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  height: 130,
-                                  width: 130,
-                                  child: ElevatedButton(
-                                      onPressed: (){
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                          primary: Colors.white,
-                                          shadowColor: Colors.grey[900],
-                                          elevation: 10,
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.circular(20)
+
+
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              // Padding(
+                              //   padding: const EdgeInsets.all(0),
+                              //   child: Column(
+                              //     crossAxisAlignment: CrossAxisAlignment.center,
+                              //     children: [
+                              //       SizedBox(
+                              //         height: 130,
+                              //         width: 130,
+                              //         child: ElevatedButton(
+                              //             onPressed: (){
+                              //               getImage(source: ImageSource.camera);
+                              //             },
+                              //             style: ElevatedButton.styleFrom(
+                              //                 primary: Colors.white,
+                              //                 shadowColor: Colors.grey[900],
+                              //                 elevation: 10,
+                              //                 shape: RoundedRectangleBorder(
+                              //                     borderRadius: BorderRadius.circular(20)
+                              //                 )
+                              //             ),
+                              //             child: Image.asset('assets/camera.png')
+                              //         ),
+                              //       ),
+                              //       Padding(
+                              //         padding: const EdgeInsets.only(bottom: 8.0, top: 10),
+                              //         child: Text(
+                              //           'Take Photo',
+                              //           textAlign: TextAlign.center,
+                              //           style: TextStyle(
+                              //             color: Colors.black,
+                              //             fontSize: 15,
+                              //             fontFamily: 'Montserrat',
+                              //           ),
+                              //         ),
+                              //       ),
+                              //     ],
+                              //   ),
+                              // ),
+                              Padding(
+                                padding: const EdgeInsets.all(0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    SizedBox(
+                                      height: 80,
+                                      width: 80,
+                                      child: ElevatedButton(
+                                          onPressed: (){
+                                            getImage(source: ImageSource.camera);
+                                          },
+                                          style: ElevatedButton.styleFrom(
+                                              primary: Colors.white,
+                                              shadowColor: Colors.grey[900],
+                                              elevation: 10,
+                                              shape: RoundedRectangleBorder(
+                                                  borderRadius: BorderRadius.circular(20)
+                                              )
+                                          ),
+                                          child: Transform.scale(
+                                              scale: 1.2,
+                                              child: Image.asset('assets/camera.png')
                                           )
                                       ),
-                                      child: Image.asset('assets/add-photo.png')
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(bottom: 8.0, top: 10),
-                                  child: Text(
-                                    'Select Photo from \nGallery',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 15,
-                                      fontFamily: 'Montserrat',
                                     ),
-                                  ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(bottom: 8.0, top: 10),
+                                      child: Text(
+                                        'Take Picture',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                          fontFamily: 'Montserrat',
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    SizedBox(
+                                      height: 80,
+                                      width: 80,
+                                      child: ElevatedButton(
+                                          onPressed: (){
+                                            getImage(source: ImageSource.gallery);
+                                          },
+                                          style: ElevatedButton.styleFrom(
+                                              primary: Colors.white,
+                                              shadowColor: Colors.grey[900],
+                                              elevation: 10,
+                                              shape: RoundedRectangleBorder(
+                                                  borderRadius: BorderRadius.circular(20)
+                                              )
+                                          ),
+                                          child: Transform.scale(
+                                              scale: 1.2,
+                                              child: Image.asset('assets/add-photo.png')
+                                          )
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(bottom: 8.0, top: 10),
+                                      child: Text(
+                                        'Select Image',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                          fontFamily: 'Montserrat',
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          SizedBox(
+                            height: 100,
+                            width: 250,
+                            child: ElevatedButton(
+                                onPressed: (){
+                                  getImage(source: ImageSource.gallery);
+                                },
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.white,
+                                    shadowColor: Colors.grey[900],
+                                    elevation: 10,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10)
+                                    )
+                                ),
+                                child: Text(
+                                  'Scan Image',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(3, 135, 96, 1.0),
+                                    fontSize: 25,
+                                    fontFamily: 'Montserrat',
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                )
                             ),
                           ),
                         ],
@@ -170,5 +288,15 @@ class _scannerState extends State<scanner> {
         ],
       ),
     );
+  }
+
+  void getImage({required ImageSource source}) async {
+    final file = await ImagePicker().pickImage(source:source);
+
+    if(file?.path != null){
+      setState(() {
+        imageFile = File(file!.path);
+      });
+    }
   }
 }
